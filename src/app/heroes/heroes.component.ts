56906efcb1a9ea5790aadad717863102dd9328e6
carlos.quadros.br <carlos.quadros.br@gmail.com>
2021-10-22 17:52:57 -0300

    Add messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import {  Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +11,22 @@ import { HEROES } from "../mock-heroes";
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
-  
+  heroes: Hero[] = [];
+ 
+  constructor(private heroService : HeroService, private messageService : MessageService) { }
+
   selectedHero?: Hero;
   onSelect(hero : Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-  constructor() { }
-
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes() : void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
